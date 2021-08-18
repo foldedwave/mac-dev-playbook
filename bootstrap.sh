cbd62eeb5075d869155acc920bb4b48259338490
foldedwave <david.fowle@foldedwave.com>
2021-08-18 21:30:54 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,12 @@
-xcode-select --install
+# Install brew now - we will use it anyway but this handily installs mac command line tools too 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 mkdir -p ~/source/ansible/
 cd ~/source/ansible
 
 git clone https://github.com/foldedwave/mac-dev-playbook.git
 
+# Do all python related commands inside a venv
 python3 -m venv ~/source/ansible/mac-dev-playbook/venv
 source venv/bin/activate
 
