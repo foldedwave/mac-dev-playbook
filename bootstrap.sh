edd80ed7338c4c2ac412293435705854a959f377
foldedwave <david.fowle@foldedwave.com>
2021-08-18 23:10:15 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,6 @@ cd ~/source/ansible
 git clone https://github.com/foldedwave/mac-dev-playbook.git
 cd mac-dev-playbook
 
-# Do all python related commands inside a venv
 python3 -m venv ~/source/ansible/mac-dev-playbook/venv
 source venv/bin/activate
 
