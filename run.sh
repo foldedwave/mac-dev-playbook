95199361f410bc7d219ff0a21043098dbe6061f3
foldedwave <david.fowle@foldedwave.com>
2021-08-18 17:15:47 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,12 @@
+xcode-select --install
+
+mkdir -p ~/source/ansible/
+cd ~/source/ansible
+
+git clone https://github.com/foldedwave/mac-dev-playbook.git
+
 python3 -m venv ~/source/ansible/mac-dev-playbook/venv
 source venv/bin/activate
-pip3 install --upgrade pip
-pip3 install ansible
+
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
