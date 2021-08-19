70ad2642d10a75ccef567432352e533aaa34c5ad
foldedwave <david.fowle@foldedwave.com>
2021-08-20 00:13:31 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
 scr=$0:A
 
+autoload colors; colors
+
 # Stop sudo from timing out
 sudo -v
 sleep 5
@@ -9,42 +11,54 @@ while true; do
     sleep 60
 done &
 
-# Install xcode command line tools
+
+# Install Command Line Tools if needed
 chomp() {
   printf "%s" "${1/"$'\n'"/}"
 }
 
-touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+xcode-select -p 1>/dev/null
 
-softwareupdate -l
+if [[ '0' == $? ]]; then
+  echo $fg[green]Command Line Tools already installed$reset_color
+else
+  echo $fg[green]Installing Command Line Tools$reset_color
+
+  touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
 
   clt_label_command="/usr/sbin/softwareupdate -l |
-                      grep -B 1 -E 'Command Line Tools' |
-                      awk -F'*' '/^ *\\*/ {print \$2}' |
-                      sed -e 's/^ *Label: //' -e 's/^ *//' |
-                      sort -V |
-                      tail -n1"
+                     grep -B 1 -E 'Command Line Tools' |
+                     awk -F'*' '/^ *\\*/ {print \$2}' |
+                     sed -e 's/^ *Label: //' -e 's/^ *//' |
+                     sort -V |
+                     tail -n1"
   clt_label="$(chomp "$(/bin/bash -c "$clt_label_command")")"
-  echo $clt_label
-
 
   if [[ -n "$clt_label" ]]; then
-    echo "Installing $clt_label"
     softwareupdate -i $clt_label
     #execute_sudo "/bin/rm" "-f" "$clt_placeholder"
     #execute_sudo "/usr/bin/xcode-select" "--switch" "/Library/Developer/CommandLineTools"
   fi
 
+  rm /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+fi
 
-rm /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
 
-
-
+# Clone this repo or update
 mkdir -p ~/source/provisioning/
 cd ~/source/provisioning
 
-git clone https://github.com/foldedwave/mac-dev-playbook.git
+if [ ! -d "./mac-dev-playbook" ]; then
+  echo $fg[green]Cloning Repo$reset_color
+  git clone https://github.com/foldedwave/mac-dev-playbook.git
+else
+  echo $fg[green]Updating Repo$reset_color
+  git pull
+fi
+
 cd mac-dev-playbook
+
+
 
 #python3 -m venv ~/source/ansible/mac-dev-playbook/venv
 #source venv/bin/activate
@@ -52,7 +66,12 @@ cd mac-dev-playbook
 #python3 -m pip install --upgrade pip
 #python3 -m pip install -r requirements.txt
 
+
+
+# Run the update scripts
 . ./update.sh
+
+
 
 # CLEANUP
 rm $scr
