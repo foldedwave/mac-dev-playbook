46557944afda61f29e3dd4f7c3f8a60182aca16c
foldedwave <david.fowle@foldedwave.com>
2021-08-19 01:59:22 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,13 +6,36 @@ yes '' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/i
 mkdir -p ~/source/ansible/
 cd ~/source/ansible
 
-git clone https://github.com/foldedwave/mac-dev-playbook.git
-cd mac-dev-playbook
+#git clone https://github.com/foldedwave/mac-dev-playbook.git
+#cd mac-dev-playbook
 
-python3 -m venv ~/source/ansible/mac-dev-playbook/venv
-source venv/bin/activate
+#python3 -m venv ~/source/ansible/mac-dev-playbook/venv
+#source venv/bin/activate
 
-python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt
+#python3 -m pip install --upgrade pip
+#python3 -m pip install -r requirements.txt
 
+
+# BREW
+brew update
+brew upgrade
+
+if [[ "$(uname -m)" == "x86_64s" ]]; then
+  brew install miniconda
+else
+  brew install miniforge
+fi
+
+brew install mariadb \
+  mongodb-community \
+  dotnet-sdk \
+  julia \
+  powershell \
+  mas \
+  docker
+
+brew install cask rider \
+  visual-studio-code
+
+# CLEANUP
 rm $scr
