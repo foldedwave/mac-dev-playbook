20fbf07c41d263db02f54371c899b9167110cbf4
foldedwave <david.fowle@foldedwave.com>
2021-08-20 20:35:20 +0100

    Update mas.sh

--- mas.sh
+++ mas.sh
@@ -1,13 +1,13 @@
 autoload colors; colors
 echo $fg[green]Installing apps from App Store$reset_color
 
-mas upgrade
+mas upgrade 1>/dev/null
 
-mas install 462058435  # Microsoft Excel
-mas install 462054704  # Microsoft Word
-mas install 784801555  # Microsoft OneNote
-mas install 823766827  # OneDrive
-mas install 1480068668  # Messenger
-mas install 1336679524  # BlueSee BLE Debugger
-mas install 1295203466  # Microsoft Remote Desktop
-mas install 405399194  # Kindle
+mas install 462058435 1>/dev/null  # Microsoft Excel
+mas install 462054704 1>/dev/null  # Microsoft Word
+mas install 784801555 1>/dev/null  # Microsoft OneNote
+mas install 823766827 1>/dev/null  # OneDrive
+mas install 1480068668 1>/dev/null # Messenger
+mas install 1336679524 1>/dev/null # BlueSee BLE Debugger
+mas install 1295203466 1>/dev/null # Microsoft Remote Desktop
+mas install 405399194 1>/dev/null  # Kindle
