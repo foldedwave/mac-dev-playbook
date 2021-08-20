bd02c1fe169bd539a97736962f5122c22fd5ecb4
foldedwave <david.fowle@foldedwave.com>
2021-08-20 01:29:08 +0100

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -1,6 +1,9 @@
-# BREW
+autoload colors; colors
+echo $fg[green]***Installing/Updating Homebrew$reset_color
+
 yes '' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+echo $fg[green]***Installing/Updating apps from Brew$reset_color
 brew update
 brew upgrade
 
