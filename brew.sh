ae3db10c6e60d422122285c5ea2b1447e70e1ed6
foldedwave <david.fowle@foldedwave.com>
2021-08-20 22:44:48 +0100

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -1,7 +1,7 @@
 autoload colors; colors
 echo $fg[green]Installing/Updating Homebrew$reset_color
 
-yes '' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" 1>/dev/null
+yes '' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" > /dev/null
 
 echo $fg[green]Installing/Updating apps from Brew$reset_color
 brew update 1>/dev/null
@@ -11,23 +11,21 @@ brew tap mongodb/brew
 brew tap homebrew/cask-fonts
 
 if [[ "$(uname -m)" == "x86_64" ]]; then
-  brew install miniconda 1>/dev/null
+  brew install --quiet miniconda
 else
-  brew install miniforge 1>/dev/null
+  brew install --quiet miniforge
 fi
 
-brew install \
+brew install --quiet \
   mariadb \
   mongodb-community \
   mas \
   powershell \
-  dotnet-sdk \
-  1>/dev/null
+  dotnet-sdk
 
-brew install --cask \
+brew install --quiet --cask \
   rider \
   visual-studio-code \
   julia \
   docker \
-  font-fira-code \
-  1>/dev/null
+  font-fira-code
