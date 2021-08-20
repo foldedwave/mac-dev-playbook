bfbe2c3e3c61228bd2bdfb0ec6ffce83ae2489bf
David Fowle <david@macos-11.5.2.shared>
2021-08-21 00:10:11 +0100

    Terminal theme

--- osx.sh
+++ osx.sh
@@ -1,4 +1,4 @@
 autoload colors; colors
 echo $fg[green]Configuring macos$reset_color
 
-defaults write ANSGlobalDomain AppleInterfaceStyle Dark
+defaults write NSGlobalDomain AppleInterfaceStyle Dark
