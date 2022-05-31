3794c4dc3662229206e4a5fc0487196fd5eb61f4
foldedwave <david.fowle@foldedwave.com>
2022-05-31 21:02:11 +0100

    Update osx.sh

--- osx.sh
+++ osx.sh
@@ -3,3 +3,4 @@ echo $fg[green]Configuring macos$reset_color
 
 defaults write NSGlobalDomain AppleInterfaceStyle Dark
 defaults delete NSGlobalDomain AppleInterfaceStyleSwitchesAutomatically
+defaults write com.apple.dock "tilesize" -int "36" && killall Dock
