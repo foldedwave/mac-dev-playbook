159e3b1aaa8c9f00989b8e11fb84b6012a14b294
foldedwave <foldedwave@fake.mail>
2022-10-12 16:06:31 +0100

    Update

---
+++ scripts/pushall.sh
@@ -0,0 +1,13 @@
+#P=$(dirname -- "$( readlink -f -- "$0"; )");
+P=$(dirname -- "$0"; );
+
+echo $P
+
+for fullfile in $P/*
+do
+  if [ -f "$fullfile/push.sh" ]
+  then
+    echo $fullfile/push.sh
+    $fullfile/push.sh
+  fi
+done
