9406edb0033caa237d5cfbd11afea7ed2e29514f
foldedwave <foldedwave@fake.mail>
2023-03-27 22:21:41 +0100

    Update

--- mas.sh
+++ mas.sh
@@ -7,9 +7,9 @@ mas upgrade
 
 mas install 462058435  # Microsoft Excel
 mas install 462054704  # Microsoft Word
-mas install 784801555  # Microsoft OneNote
-mas install 823766827  # OneDrive
-mas install 1480068668 # Messenger
+#mas install 784801555  # Microsoft OneNote
+#mas install 823766827  # OneDrive
+#mas install 1480068668 # Messenger
 mas install 1336679524 # BlueSee BLE Debugger
 mas install 1295203466 # Microsoft Remote Desktop
 mas install 405399194  # Kindle
