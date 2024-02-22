0b789e8d76d2a3b8cc378809217802ba2b861a3a
David Fowle <david@davids-air.home>
2024-02-22 18:36:57 +0000

    Added rust-analyser

--- brew.sh
+++ brew.sh
@@ -74,3 +74,4 @@ brew install --quiet mactex
 brew install --quiet emacs-plus@29 --with-imagemagick --with-native-comp --with-elrumo2-icon
 
 brew install --quiet rustup-init
+brew install --quiet rust-analyser
